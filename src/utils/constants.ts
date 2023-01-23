413448ceca086c4a6fb12084052854501d193ca0
Daniel <danieladammiller@gmail.com>
2023-01-23 13:41:31 -0800

    finibot

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,17 +1,17 @@
 export default {
   RESERVOIR_ICON:
-    "https://cdn.discordapp.com/icons/872790973309153280/0dc1b70867aeeb2ee32563f575c191c6.webp?size=4096",
+    "https://cdn.finiliar.com/website/finiliar-logo.gif",
   ALERT_COOLDOWN: 60 * 30, // 30 minute cooldown
   PRICE_CHANGE_OVERRIDE: 0.1, // 10% price change
-  ALERT_ENABLED: { listings: false, sales: false, floor: true, bid: true }, // enable alerts
+  ALERT_ENABLED: { listings: true, sales: true, floor: true, bid: true }, // enable alerts
   TRACKED_CONTRACTS: [],
   CHANNEL_IDS: {
-    mainChannel: "1037386291781840977",
-    listingChannel: "11111111111111111111",
-    salesChannel: "11111111111111111111",
+    mainChannel: "1005812542142164993",
+    listingChannel: "1010246029775478887",
+    salesChannel: "926299434285928479",
   },
-  ALERT_CONTRACT: "0x521f9c7505005cfa19a8e5786a9c3c9c9f5e6f42",
-  APPLICATION_ID: "1033117008667099157",
+  ALERT_CONTRACT: "0x5a0121a0a21232ec0d024dab9017314509026480",
+  APPLICATION_ID: "1067161068050329732",
   REDIS_HOST: "127.0.0.1",
   REDIS_PORT: 6379,
 };
